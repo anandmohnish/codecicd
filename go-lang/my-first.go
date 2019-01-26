18d454c6f0e73eddcb5ad89db61469cc525b7c39
anandmohnish <manand1@hawk.iit.edu>
2019-01-26 18:31:39 -0500

    You are doing great...

---
+++ go-lang/my-first.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n, err := fmt.Println("Drunk Coding by Mohnish", at, 12, midnight)
+	fmt.Println("The number of characters printed are", n)
+	fmt.Println("The error is", err)
+
+}
